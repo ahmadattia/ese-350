953787223e3b3b1b068562844da9e1764dc64b46
David Cantu <davigfre@gmail.com>
2016-01-28 16:56:46 -0500

    Part 1 Finished

--- morse.c
+++ morse.c
@@ -11,37 +11,37 @@ unsigned long pulse_width;
 	
 
 ISR(TIMER1_CAPT_vect){
-	TIFR1 |= 0x20;
-	//char greetingsc = 'c';
-	//printf("%c\n", greetingsc);
+	// if(PINB & 0x01){
+	// 	PORTB ^= 0x20;
+	// }
+	TCCR1B ^= 0x40;
 	PORTB ^= 0x20;
-	
-	//cli();
+
 }
 
 
 
 int main(void)
 {
-	//PORTB	^=	0xFF;
-	
+
+	DDRB |= 0x20; //This turns the PORTB fifth bit to output.
 	uart_init();
 	TCCR1B |= 0x01; //enable timer
 	TCCR1B |= 0x40; //capture rising edge
 	TIMSK1 |= 0x20; //enable interrupt
 	TIFR1 |= 0x20; // clear the input flag
 	sei(); // enable all interrupts.
-	while ((TIFR1 & 0x20));
+	// while (!(TIFR1 & 0x20));
 	
-	edge1 = ICR1;
-	TCCR1B &= 0xBF;
-	TIFR1 |= 0x20;
+	// edge1 = ICR1;
+	// TCCR1B &= 0xBF;
+	// TIFR1 |= 0x20;
 	
-	while((TIFR1 & 0x20));
-	edge2 = ICR1;
+	// while(!(TIFR1 & 0x20));
+	// edge2 = ICR1;
 	
 
 	
-	printf("pulse width = %int\n", edge1);
+	//printf("pulse width = %int\n", edge1);
 	for(;;);
 }
